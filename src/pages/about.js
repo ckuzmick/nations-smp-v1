ce63ed038f434c98d251a5f72517c8a8337b67ff
ck <ck@cks-macbook-pro.lan>
2021-02-24 12:19:51 -0500

    hiuda

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import React from "react"
 import NavBar from "../components/layout/nav-bar.js"
-
+import "@fontsource/montserrat"
 
 const About = () => (
   <div style={{
@@ -8,8 +8,20 @@ const About = () => (
   }}
   >
       <NavBar />
-    <h1>About Us</h1>
-    <p>We are an SMP</p>
+    <h1 style={{
+      fontFamily: "Montserrat",
+    }}    
+    >About Us</h1>
+    <p style={{
+      fontFamily: "Montserrat",
+      fontSize: "20px"
+    }}
+    
+    >We are a group of friends who play on a seudo-factions server called Nations SMP. There are countries, wars, builds, and lots of fun! If you would like to know more about how the server works exactly you should go to our Laws & Rules page!</p>
+    <h2 style={{
+      fontFamily: "Montserrat"
+    }}
+    >Here are our youtube channels!</h2>
   </div>
 )
 
