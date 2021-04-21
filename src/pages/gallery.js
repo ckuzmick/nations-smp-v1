308010fcb6e044cd9c444771aa1c93c4dceac10c
ck <ck@cks-macbook-pro.lan>
2021-04-21 10:37:31 -0400

    cool

---
+++ src/pages/gallery.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+import NavBar from "../components/layout/nav-bar.js"
+
+const ComponentTest = () => (
+  <div>
+      <Helmet title="Gallery | Nations SMP"></Helmet> 
+      <NavBar />
+
+      
+  </div>
+)
+
+export default GalleryPage
